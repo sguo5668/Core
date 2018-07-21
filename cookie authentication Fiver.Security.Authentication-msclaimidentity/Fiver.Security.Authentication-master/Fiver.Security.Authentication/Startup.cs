d90eb5f909441eb163a7357ede3171b1d3bc0289
song guo <sguo5668@gmail.com>
2018-07-20 21:01:21 -0700

    cookie authen

---
+++ cookie authentication Fiver.Security.Authentication-msclaimidentity/Fiver.Security.Authentication-master/Fiver.Security.Authentication/Startup.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Fiver.Security.Authentication
+{
+    public class Startup
+    {
+        public void ConfigureServices(
+            IServiceCollection services)
+        {
+            services.AddAuthentication("FiverSecurityScheme")
+                    .AddCookie("FiverSecurityScheme", options =>
+                    {
+                        options.AccessDeniedPath = new PathString("/Security/Access");
+                        options.Cookie = new CookieBuilder
+                        {
+                            //Domain = "",
+                            HttpOnly = true,
+                            Name = ".Fiver.Security.Cookie",
+                            Path = "/",
+                            SameSite = SameSiteMode.Lax,
+                            SecurePolicy = CookieSecurePolicy.SameAsRequest
+                        };
+                        options.Events = new CookieAuthenticationEvents
+                        {
+                            OnSignedIn = context =>
+                            {
+                                Console.WriteLine("{0} - {1}: {2}", DateTime.Now,
+                                  "OnSignedIn", context.Principal.Identity.Name);
+                                return Task.CompletedTask;
+                            },
+                            OnSigningOut = context =>
+                            {
+                                Console.WriteLine("{0} - {1}: {2}", DateTime.Now,
+                                  "OnSigningOut", context.HttpContext.User.Identity.Name);
+                                return Task.CompletedTask;
+                            },
+                            OnValidatePrincipal = context =>
+                            {
+                                Console.WriteLine("{0} - {1}: {2}", DateTime.Now,
+                                  "OnValidatePrincipal", context.Principal.Identity.Name);
+                                return Task.CompletedTask;
+                            }
+                        };
+                        //options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                        options.LoginPath = new PathString("/Security/Login");
+                        options.ReturnUrlParameter = "RequestPath";
+                        options.SlidingExpiration = true;
+                    });
+
+            services.AddMvc();
+        }
+
+        //public void ConfigureServices(
+        //    IServiceCollection services)
+        //{
+        //    services.AddAuthentication("FiverSecurityScheme")
+        //            .AddCookie("FiverSecurityScheme", options =>
+        //            {
+        //                options.AccessDeniedPath = new PathString("/Security/Access");
+        //                options.LoginPath = new PathString("/Security/Login");
+        //            });
+
+        //    services.AddMvc();
+        //}
+
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
