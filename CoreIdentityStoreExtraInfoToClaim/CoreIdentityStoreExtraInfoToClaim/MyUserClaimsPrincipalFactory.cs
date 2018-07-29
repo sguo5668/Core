815342518d863f2d8423c5d3b669bc1834822bdf
song guo <sguo5668@gmail.com>
2018-07-29 14:30:40 -0700

    core identity add addtional data to claim

---
+++ CoreIdentityStoreExtraInfoToClaim/CoreIdentityStoreExtraInfoToClaim/MyUserClaimsPrincipalFactory.cs
@@ -0,0 +1,29 @@
+﻿using CoreIdentityStoreExtraInfoToClaim.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CoreIdentityStoreExtraInfoToClaim
+{
+    public class MyUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
+    {
+        public MyUserClaimsPrincipalFactory(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IOptions<IdentityOptions> optionsAccessor)
+            : base(userManager, roleManager, optionsAccessor)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("ContactName", user.ContactName ?? ""));
+            return identity;
+        }
+    }
+}
