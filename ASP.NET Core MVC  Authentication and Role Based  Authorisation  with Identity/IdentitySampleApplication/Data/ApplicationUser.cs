06942cdc3070cc9ebdd1cbea988d72b941fd7a75
song guo <sguo5668@gmail.com>
2018-07-29 21:52:20 -0700

    role based authorize

---
+++ ASP.NET Core MVC  Authentication and Role Based  Authorisation  with Identity/IdentitySampleApplication/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace IdentitySampleApplication.Data
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
