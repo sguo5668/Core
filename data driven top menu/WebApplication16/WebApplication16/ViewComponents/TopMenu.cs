cb5a8c46e9f322c2d100c380d8a2723e4b052673
song guo <sguo5668@gmail.com>
2018-08-01 19:06:18 -0700

    menu use  viewcomponent

---
+++ data driven top menu/WebApplication16/WebApplication16/ViewComponents/TopMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication16.ViewComponents
+{
+	public class TopMenu : ViewComponent
+	{
+		private readonly TopMenuDbContext _context;
+
+		public TopMenu(TopMenuDbContext context)
+		{
+			_context = context;
+
+			_context.AddMenuItems();
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var model = _context.MenuItems.ToList();
+
+			return await Task.FromResult((IViewComponentResult)View("Default", model));
+		}
+	}
+}
