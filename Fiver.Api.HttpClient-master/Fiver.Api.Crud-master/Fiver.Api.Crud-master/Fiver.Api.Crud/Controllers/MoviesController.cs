2ca1b1d7791d10df0858609d7b46fea547db6533
song guo <sguo5668@gmail.com>
2018-08-18 15:42:06 -0700

    consume web api using httpclient

---
+++ Fiver.Api.HttpClient-master/Fiver.Api.Crud-master/Fiver.Api.Crud-master/Fiver.Api.Crud/Controllers/MoviesController.cs
@@ -0,0 +1,158 @@
+﻿using Fiver.Api.Crud.Lib;
+using Fiver.Api.Crud.Models.Movies;
+using Fiver.Api.Crud.OtherLayers;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fiver.Api.Crud.Controllers
+{
+    [Route("movies")]
+    public class MoviesController : BaseController
+    {
+        private readonly IMovieService service;
+
+        public MoviesController(IMovieService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var model = service.GetMovies();
+
+            var outputModel = ToOutputModel(model);
+            return Ok(outputModel);
+        }
+
+        [HttpGet("{id}", Name = "GetMovie")]
+        public IActionResult Get(int id)
+        {
+            var model = service.GetMovie(id);
+            if (model == null)
+                return NotFound();
+
+            var outputModel = ToOutputModel(model);
+            return Ok(outputModel);
+
+            //var model = service.GetMovie(id);
+            //return OkOrNotFound(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]MovieInputModel inputModel)
+        {
+            if (inputModel == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return Unprocessable(ModelState);
+
+            var model = ToDomainModel(inputModel);
+            service.AddMovie(model);
+
+            var outputModel = ToOutputModel(model);
+            return CreatedAtRoute("GetMovie", new { id = outputModel.Id }, outputModel);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]MovieInputModel inputModel)
+        {
+            if (inputModel == null || id != inputModel.Id)
+                return BadRequest();
+
+            if (!service.MovieExists(id))
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return new UnprocessableObjectResult(ModelState);
+
+            var model = ToDomainModel(inputModel);
+            service.UpdateMovie(model);
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdatePatch(
+            int id, [FromBody]JsonPatchDocument<MovieInputModel> patch)
+        {
+            if (patch == null)
+                return BadRequest();
+
+            var model = service.GetMovie(id);
+            if (model == null)
+                return NotFound();
+
+            var inputModel = ToInputModel(model);
+            patch.ApplyTo(inputModel);
+
+            TryValidateModel(inputModel);
+            if (!ModelState.IsValid)
+                return new UnprocessableObjectResult(ModelState);
+
+            model = ToDomainModel(inputModel);
+            service.UpdateMovie(model);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!service.MovieExists(id))
+                return NotFound();
+
+            service.DeleteMovie(id);
+
+            return NoContent();
+        }
+
+        #region " Mappings "
+
+        private MovieOutputModel ToOutputModel(Movie model)
+        {
+            return new MovieOutputModel
+            {
+                Id = model.Id,
+                Title = model.Title,
+                ReleaseYear = model.ReleaseYear,
+                Summary = model.Summary,
+                LastReadAt = DateTime.Now
+            };
+        }
+
+        private List<MovieOutputModel> ToOutputModel(List<Movie> model)
+        {
+            return model.Select(item => ToOutputModel(item))
+                        .ToList();
+        }
+
+        private Movie ToDomainModel(MovieInputModel inputModel)
+        {
+            return new Movie
+            {
+                Id = inputModel.Id,
+                Title = inputModel.Title,
+                ReleaseYear = inputModel.ReleaseYear,
+                Summary = inputModel.Summary
+            };
+        }
+
+        private MovieInputModel ToInputModel(Movie model)
+        {
+            return new MovieInputModel
+            {
+                Id = model.Id,
+                Title = model.Title,
+                ReleaseYear = model.ReleaseYear,
+                Summary = model.Summary
+            };
+        }
+        
+        #endregion
+    }
+}
---
+++ Fiver.Api.HttpClient-master/Fiver.Api.Crud-master/Fiver.Api.Crud-master/Fiver.Api.Crud/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Fiver.Api.Crud.OtherLayers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fiver.Api.Crud
+{
+    public class Startup
+    {
+        public void ConfigureServices(
+            IServiceCollection services)
+        {
+            services.AddSingleton<IMovieService, MovieService>();
+            services.AddSingleton<IReviewService, ReviewService>();
+
+            services.AddMvc();
+        }
+
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env)
+        {
+            app.UseExceptionHandler(configure =>
+            {
+                configure.Run(async context =>
+                {
+                    var ex = context.Features
+                                    .Get<IExceptionHandlerFeature>()
+                                    .Error;
+
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync($"{ex.Message}");
+                });
+            });
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
